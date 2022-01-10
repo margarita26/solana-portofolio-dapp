9dea94d3354ab35bc489489fe208651849c3e6b8
Margarita Gubanova <gubanova.marg@yandex.com>
2022-01-09 22:05:47 -0500

    resolve minutes

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,18 +1,18 @@
 import { format } from "date-fns";
 
-const datesAreOnSameDay = (first: Date, second: Date) =>
+const datesAreOnSameDay = (first: Date, second: Date): boolean =>
   first.getFullYear() === second.getFullYear() &&
   first.getMonth() === second.getMonth() &&
   first.getDate() === second.getDate();
 
-export const getDate = (unixTimeStamp: number | null | undefined) => {
+export const getDate = (unixTimeStamp: number | null | undefined): string => {
   if (unixTimeStamp) {
     const milisecconds = unixTimeStamp * 1000;
     const todayDate = new Date();
     const messagedDate = new Date(milisecconds);
 
     const areDaysEqual = datesAreOnSameDay(todayDate, messagedDate);
-    return areDaysEqual? format(messagedDate, "HH:dd") : format(messagedDate, "MMM dd");
+    return areDaysEqual? format(messagedDate, "HH:mm") : format(messagedDate, "MMM dd");
   }
   return "";
 };
