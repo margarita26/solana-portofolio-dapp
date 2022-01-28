a7521d9c474b1d5bfe541797dd345aafc56769c9
Margarita Gubanova <gubanova.marg@yandex.com>
2022-01-28 18:07:02 -0500

    switch to m mainnet

--- src/solana/Wallet.tsx
+++ src/solana/Wallet.tsx
@@ -21,7 +21,7 @@ interface WalletProps {
 
 const Wallet = ({children}: WalletProps) => {
     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-    const network = WalletAdapterNetwork.Devnet;
+    const network = WalletAdapterNetwork.Mainnet;
 
     // You can also provide a custom RPC endpoint.
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
